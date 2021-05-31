1343e8398df5e8c2d3f0ecf331ca9d0d07dc3dbc
Adarsh Maurya <48577949+adarshavim@users.noreply.github.com>
2021-05-31 12:51:40 +0530

    removing config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,10 +1,1 @@
-export const config = {
-  apiKey: "AIzaSyDMOaA7LtRQK9ojD0mrwa03j-rSysaeKGY",
-  authDomain: "plant-disease-detection-35389.firebaseapp.com",
-  databaseURL: "https://plant-disease-detection-35389.firebaseio.com",
-  projectId: "plant-disease-detection-35389",
-  storageBucket: "plant-disease-detection-35389.appspot.com",
-  messagingSenderId: "106519222770",
-  appId: "1:106519222770:web:66eaec7c7cbe79535fb36b",
-  measurementId: "G-Z65JWGMTZ2"
-};
+
